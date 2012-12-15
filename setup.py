667a25bcbc4b5786882d934d78d56edf564637e8
justin <heartlessjunkie@gmail.com>
2012-12-15 15:55:38 -0800

    updated for first pip release

--- setup.py
+++ setup.py
@@ -1,16 +1,38 @@
-from distutils.core import setup
+from setuptools import setup
+
+requires = []
+
+try:
+    import argparse
+except ImportError:
+    requires.append('argparse')
+
+entry_points = {
+    'console_scripts': [
+        'ddg = ddg:main',
+   ]
+}
 
 setup(
     name='ddg',
-    version='0.1.0',
+    version='0.1.1',
     author='justinls',
     author_email='heartlessjunkie@gmail.com',
-    packages=['ddg, ddg.duckduckgo'],
+    packages=['duckduckgo'],
+    scripts=['ddg.py'],
     url='https://github.com/justinls/ddg',
     license='LICENSE.txt',
     description='www.duckduckgo.com zero click api for your shell',
-    long_description=open('README.rst').read(),
-    install_requires=[
-        "Python >= 2.7.3",
+    long_description=open('README').read(),
+    install_requires=requires,
+    entry_points=entry_points,
+    classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Environment :: Console',
+         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python :: 2.7',
+         'Topic :: Internet :: WWW/HTTP',
+         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
