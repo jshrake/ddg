7e8c62f9aed03fe2e3244e015b60618cb0d8720e
justin <justinshrake@gmail.com>
2012-12-16 15:47:05 -0800

    upload new version to pypi for the new trove classifier

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup(
     name='ddg',
-    version='0.1.6',
+    version='0.1.7',
     author='justinls',
     author_email='heartlessjunkie@gmail.com',
     packages=['duckduckgo'],
